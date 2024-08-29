39fc743053f0c63805f676d3b05176d6d076cafc
Nick Bobertz <Nbobertz@gmail.com>
2024-08-28 21:12:20 -0400

    not much

--- window.py
+++ window.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter.ttk import *
-import miniaudio
+import pafy
 
 #the idea is to branch this button out to another ifle where we can listen to a random piece of stalker music from the game and radio stations.
 class NewWindow(Toplevel):
@@ -14,12 +14,12 @@ class NewWindow(Toplevel):
         label.configure(background='red')
         self.configure(background='blue')
 
-
-        url = 'https://www.radio.net/s/freeukraine'
-        with miniaudio.IceCastClient(url) as source:
-            stream=miniaudio.stream_any(source,source.audio_format)
-            with miniaudio.PlaybackDevice() as device:
-                device.start(stream)
+        #steram the audio
+        video = pafy.new(url='https://www.youtube.com/watch?v=h4VJGNNSQnw&list=RDMMimBlPXbAv6E&index=3',ydl_opts={'nocheckcertificate':True})
+        print(video)
+        best=video.getbestaudio()
+        stream_urls=best.url
+        print(stream_urls)
 
         #get around the above error with Selenium browser addon
 
